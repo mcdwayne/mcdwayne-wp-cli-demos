9703e261feae5d68d600a85bb10b1d96eb6df1fc
Dwayne McDaniel <1dwayne.mcdaniel@gmail.com>
2017-03-23 19:26:46 -0700

    cleaned up for #WCSD talk

--- WP-CLI-script.sh
+++ WP-CLI-script.sh
@@ -17,6 +17,10 @@ chmod +x wp-cli.phar
 # set file to path :$wp
 sudo mv wp-cli.phar /usr/local/bin/wp
 
+# WP-CLI is not installed!!!!
+# Now, what do we do with it?
+
+# ------CORE------
 
 #  download WordPress
 wp core download
@@ -27,11 +31,14 @@ wp core config --dbname=c9 --dbuser=mcdwayne
 # run install 
 wp core install --url=wpcliexamplesite1.c9users.io --title=WP-CLI_DEMO_Test --admin_user=dwayne --admin_password=Password1 --admin_email=dwayne@pantheon.io
 
+
+# ------SEARCH-AND-REPLACE------
+
 # fix the links with search and replace
 wp search-replace 'wpcliexamplesite1'  'wpcliexamplesite1-mcdwayne' 
 
-# Turn off the widgets.  All of them
-wp widget reset --all
+
+# ------POSTS------
 
 # generate some dummy posts
 wp post generate --count=15 --post_date=2001-01-01
@@ -39,9 +46,21 @@ wp post generate --count=15 --post_date=2001-01-01
 # even better let's pull down some LorIpsum filler text and make dummy posts with that
 curl loripsum.net/api/5/short/headers/ul/bq | wp post generate --post_content --count=5
 
+
+# ------USERS------
+
 # create users with different roles
 wp user create bob bob@example.com --role=author
 wp user create jane jane@example.com --user_pass=“password” --role=administrator
+
+# Delete Bob
+wp user delete bob 
+
+
+# ------THEMES------
+
+# List current themes
+wp theme list
 
 # Install a new theme
 wp theme install https://downloads.wordpress.org/theme/primer.1.3.0.zip 
@@ -52,16 +71,38 @@ wp theme activate primer
 # Let's install a new theme and go ahead and activate it
 wp theme install universal --activate
 
+# We can create our own child themes pretty quickly
+wp scaffold child-theme universal_child_theme --parent_theme=universal --activate
+
+# We can also modify the theme settings
+# Set the background color to #BADA55
+wp theme mod set background_color BADA55
+
+# set the tagline to something else
+wp option update blogdescription "Thanks for watching my demo of WP-CLI!"
+
+
+
+#------PLUGINS------#
+
 # Let's install WooCommerce and activate it
 wp plugin install woocommerce
 wp plugin activate woocommerce
 
-# Let's install Jetpack and activate it
-wp plugin install jetpack --activate
+wp plugin install wordfence duplicator --activate
 
-# wp plugin install wordfence duplicator
+# Let's install an old versuon of Jetpack and activate it
+wp plugin install jetpack --version=4.3 --activate
+
+#nevermind, let's update it!
+wp plugin update jetpack
 
 # wp plugin install WHAT ARE YOUR FAVES?
+
+
+
+
+#------Menus------#
 
 # What menus exist and are active?
 wp menu list
@@ -73,26 +114,26 @@ wp menu create "my-menu"
 wp menu location assign my-menu primary
 
 # Let's go ahead and make that menu link to a known address (google in this case)
-wp menu item add-custom my-menu Google http://google.com 
+wp menu item add-custom my-menu Google http://www.google.com 
 
-#Install the CIA Emoji widget
+
+# ------WIDGETS------
+
+# Turn off the widgets.  All of them.  
+wp widget reset --all
+
+#Install the CIA Emoji widget plugin
  wp plugin install https://github.com/1dwaynemcdaniel/CIA-Emoji-WP-Plugin/archive/master.zip --activate
  
 # Set the CIA Emoji widget into the Home Sidebar
 wp widget add cia_emoji ps2
 
-# Set the background color to #BADA55
-wp theme mod set background_color BADA55
-
-# set the tagline to something else
-wp option update blogdescription "Thanks for watching my demo of WP-CLI!"
-
-
-
-# Terminus commands 
-# terminus remote:wp wpclidemosite22.dev -- plugin update --all --dry-run
-# terminus remote:wp wpclidemosite22.dev -- theme update --all --dry-run
 
 # Kalabox commands
 # kbox in front of anything
+
+# Terminus commands 
+# terminus remote:wp disruptmusicpodcast.dev -- plugin list
+# terminus remote:wp disruptmusicpodcast.dev -- theme list
+
 
