f971c0f44662ff257c41b34f83765d5b364bcf8b
mcdwayne <1dwayne.mcdaniel@gmail.com>
2018-03-05 13:37:09 -0800

    Updated branch caw to use codeanywhere and do some slick stuff

--- WP-CLI-script.sh
+++ WP-CLI-script.sh
@@ -2,9 +2,6 @@
 
 # wpcliexamplesite1
 
-# install mysql (specific step for to C9, most of time this will already be installed)
-mysql-ctl install
-
 # Download the WP-CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
@@ -24,32 +21,53 @@ sudo mv wp-cli.phar /usr/local/bin/wp
 
 #  download WordPress
 # NOW NORMALLY it is just 
-# wp core download
-
-# But currently, on some linux systems (like C9.io) with permissions around en_US locale, need to use....
-wp --allow-root core download --locale=en_GB 
+wp core download
 
 # build the wp-config.php file
 # OK, but how do we do that?  Let's ask the WP-CLI to guide us via --prompt
 # wp core config --prompt
 
 # Well actually, since I am using C9 all I need are the following
-wp core config --dbname=c9 --dbuser=mcdwayne
+wp core config --dbname=mysql --dbuser=root
 
 # Run install 
-wp core install --url=wpcliexamplesite1.c9users.io --title=WP-CLI_DEMO_Test --admin_user=dwayne --admin_password=Password1 --admin_wp_email=dwayne@pantheon.io
+wp core install --url=http://wp-cli-1dwaynemcdaniel657003.codeanyapp.com/ --title=WP-CLI --admin_user=dwayne --admin_password=Password1 --admin_email=1dwayne.mcdaniel@gmail.com
 
 
 # ------SEARCH-REPLACE------
 
 # Fix the links with search and replace, well not yet
-wp search-replace 'wpcliexamplesite1'  'wpcliexamplesite1-mcdwayne' --dry-run
+wp search-replace 'Hello world'  'This is the first demo post' --dry-run
 
 # OK, now fix the links with search and replace
-wp search-replace 'wpcliexamplesite1'  'wpcliexamplesite1-mcdwayne'
+wp search-replace 'Hello world'  'This is the first demo post'
+
+# That was fun.  Again!  BUt this time instead of the DB let's look through PHP
+wp search-replace 'Just another WordPress site'  'WCMIA ROCKS!!!!!' --precise
 
 # Let's not make changes to our live DB, instead pipe those changes to a new DB copy
-wp search-replace 'wpcliexamplesite1'  'wpcliexamplesite1-mcdwayne' --export > mysqldump.sql
+wp search-replace 'http://wp-cli-demo-box-1dwaynemcdaniel657003.codeanyapp.com/'  'https://testsite.com' --export > mysqldump.sql
+
+# ------THEME------
+
+# List current themes
+wp theme list
+
+# Let's install a new theme and activate it
+wp theme install 90s-retro --activate
+
+# We could have also activated it seperately
+# wp theme activate 90s-retro 
+
+# We can create our own child themes pretty quickly
+# wp scaffold child-theme 2016-child --parent_theme=twentysixteen
+
+# We can also modify the theme settings
+# Set the background color to #BADA55
+wp theme mod set background_color BADA55
+
+# Let's delete everything I am not using (just the parent and child themes left alone)
+wp theme delete twentyseventeen twentyfifteen
 
 
 # ------POST------
@@ -58,7 +76,7 @@ wp search-replace 'wpcliexamplesite1'  'wpcliexamplesite1-mcdwayne' --export > m
 wp post generate --count=15 --post_date=2001-01-01
 
 # Even better let's pull down some LorIpsum filler text and make dummy posts with that
-curl loripsum.net/api/5/short/headers/ul/bq | wp post generate --post_content --count=5
+curl http://baseballipsum.apphb.com/api/?paras=10 | wp post generate --post_content --count=5
 
 # Let's see what posts exists, and what metadata is exposed
 wp post list 
@@ -71,6 +89,66 @@ wp post delete 1
 
 # Let's delete these in one easy pass:
 # wp site empty
+
+# OK, let's make this a bit more interesting
+# but first we need a plugin:
+wp plugin install blackjack-lite --activate
+
+
+# now create a post with the short tag for that plugin capability
+wp post create --post_content=' [blackjack] ' --post_status=publish
+
+# AGAIN!  
+wp plugin install magic-food --activate
+wp post create --post_content=' [magicfood] ' --post_status=publish
+
+
+
+
+#------Menu------#
+
+# What menus exist and are active?
+wp menu list
+
+# let's make a new menu
+wp menu create "my-menu"
+
+# let's make that the primary menu
+wp menu location assign my-menu main-menu
+
+# Let's go ahead and make that menu link to a known address (google in this case)
+wp menu item add-custom my-menu mcdwayne.com https://www.mcdwayne.com 
+
+wp menu item add-custom my-menu twitter https://twitter.com/McDwayne
+
+wp menu item add-custom my-menu Google https://www.google.com 
+
+wp menu item add-custom my-menu Email mailto:1dwayne.mcdaniel@gmail.com
+
+#------PLUGIN------#
+
+# Let's install Yoast and activate it
+wp plugin install wp-cfm --activate
+
+
+# Let's install an old versuon of Jetpack and activate it
+wp plugin install jetpack --version=4.3 --activate
+
+#nevermind, I want the latest one.
+## Let's update it!
+### Wait, what version will this actually update to?
+wp plugin update jetpack --dry-run
+
+# I am good with that so let's actually do it:
+wp plugin update jetpack
+
+# Update all the plugins!
+# wp plugin update --all 
+
+# Want to make your own plugin the right way?  
+wp scaffold plugin newplugin
+
+
 
 # ------USER------
 
@@ -98,49 +176,6 @@ wp user add-role bob editor
 # wp user delete bob 
 
 
-# ------THEME------
-
-# List current themes
-wp theme list
-
-# Let's install a new theme
-wp theme install universal 
-
-#And go ahead and activate it
-wp theme activate universal
-
-# We can create our own child themes pretty quickly
-wp scaffold child-theme universal_child_theme --parent_theme=universal --activate
-
-# We can also modify the theme settings
-# Set the background color to #BADA55
-wp theme mod set background_color BADA55
-
-# Let's delete everything I am not using (just the parent and child themes left alone)
-wp theme delete twentysixteen twentyseventeen twentyfifteen
-
-#------PLUGIN------#
-
-# Let's install Yoast and activate it
-wp plugin install wordpress-seo --activate
-
-# Let's install an old versuon of Jetpack and activate it
-wp plugin install jetpack --version=4.3 --activate
-
-#nevermind, I want the latest one.
-## Let's update it!
-### Wait, what version will this actually update to?
-wp plugin update jetpack --dry-run
-
-# I am good with that so let's actually do it:
-wp plugin update jetpack
-
-# Update all the plugins!
-# wp plugin update --all 
-
-# Want to make your own plugin the right way?  
-wp scaffold plugin newplugin
-
 
 # -----Database--------
 # Let's export the current DB
@@ -153,46 +188,7 @@ wp db export newbackup.sql
 # wp db import newbackup.sql
 
 
-#----Other Commands---!!!!!!!
-
-
-#------Menu------#
-
-# What menus exist and are active?
-wp menu list
-
-# let's make a new menu
-wp menu create "my-menu"
-
-# let's make that the primary menu
-wp menu location assign my-menu primary
-
-# Let's go ahead and make that menu link to a known address (google in this case)
-wp menu item add-custom my-menu Google http://www.google.com 
-
-
-# ------WIDGET------
-
-# Turn off the widgets.  All of them.  
-wp widget reset --all
-
-# Install the CIA Emoji widget plugin
- wp plugin install https://github.com/1dwaynemcdaniel/CIA-Emoji-WP-Plugin/archive/master.zip --activate
- 
-# Set the CIA Emoji widget into the Home Sidebar
-wp widget add cia_emoji ps2
-
-
 # ------OPTION------
 
 # set the tagline to something that is not "Just another WordPress website"
 wp option update blogdescription "Thanks for watching my demo of WP-CLI!"
-
-# Kalabox commands
-# kbox in front of anything
-
-# Terminus commands 
-# terminus remote:wp disruptmusicpodcast.dev -- plugin list
-# terminus remote:wp disruptmusicpodcast.dev -- theme list
-
-
